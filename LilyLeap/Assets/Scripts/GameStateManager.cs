bade834e26e6d83689e7d896bec025da3a71dbb7
Anthony Dirago <diragoa@gmail.com>
2015-09-14 12:30:39 -0400

    Created a gamestatemanager script. Adjusted the scene to make the transform numbers a little easier to handle. Unity Ads was deleted and will need to be added at a later date.

---
+++ LilyLeap/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Game State manager to deal with all states of the game.
+/// Set the game state with "GameStateManager.Instance.SetGameState (GameState.GAME);"
+/// Get the game state with "GameStateManager.Instance.gameState;"
+/// </summary>
+
+
+
+public enum GameState {MAIN_MENU, START, GAME, DEATH, STORE }
+
+
+public class GameStateManager : MonoBehaviour {
+
+	protected GameStateManager(){}
+	private static GameStateManager instance = null;
+	public GameState gameState{ get; private set;}
+
+	//Allows the getting of the gamestate variable
+	public static GameStateManager Instance {
+		get{
+			if(GameStateManager.instance == null){
+				DontDestroyOnLoad(GameStateManager.instance);
+				GameStateManager.instance = new GameStateManager();
+			}
+			return GameStateManager.instance;
+		}
+	}
+
+	public void SetGameState(GameState state){
+		this.gameState = state;
+	}
+
+
+	public void OnApplicationQuit()
+	{
+		GameStateManager.instance = null;
+	}
+}
