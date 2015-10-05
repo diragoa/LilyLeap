c7f3f0f87026b91d5020c34e4d1205107214da80
JasonCohen <jcohen31313@gmail.com>
2015-10-05 16:47:32 -0400

    added a facebook button that signs players in

---
+++ LilyLeap/Assets/Scripts/FaceBook/FBholder.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class FBholder : MonoBehaviour 
+{
+	public GameObject isLoggedIn;
+	public GameObject isNotLoggedIn;
+
+	void Awake()
+		{
+			Facebook.Unity.FB.Init (SetInit, OnHideUnity);
+		}
+
+	private void SetInit()
+	{
+		Debug.Log ("FB INIT DONE.");
+
+		if(Facebook.Unity.FB.IsLoggedIn)
+		{
+			Debug.Log("LOGGED IN");
+			FBMenus(true);
+		}
+		else 
+		{
+			FBMenus(false);
+		}
+	}
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		if(!isGameShown)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+	}
+
+	public void FBLogIn()
+	{
+		Facebook.Unity.FB.LogInWithReadPermissions (new List<string>(){ "email"},AuthCallback);
+	}
+
+	void AuthCallback(Facebook.Unity.IResult result)
+	{
+		if(Facebook.Unity.FB.IsLoggedIn)
+		{
+			FBMenus(true);
+			Debug.Log("LOGGED IN WORKED");
+		}
+		else 
+		{
+			FBMenus(false);
+			Debug.Log("LOGGIN FAILED");
+		}
+	}
+
+	void FBMenus(bool loggedIn)
+	{
+		if(loggedIn)
+		{
+			isLoggedIn.SetActive(true);
+			isNotLoggedIn.SetActive(false);
+		}
+		else
+		{
+			isLoggedIn.SetActive(false);
+			isNotLoggedIn.SetActive(true);
+		}
+	}
+}
